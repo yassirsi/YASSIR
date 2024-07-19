12e7a0e02905c2ac58463eb848f2c715ef997396
yassirsi <150948131+yassirsi@users.noreply.github.com>
2024-07-19 16:17:43 +0300

    1 commit

---
+++ UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.CarParking.Repository;
+
+import com.example.CarParking.Model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByUsername(String username);
+}
